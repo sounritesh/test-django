0303989b6b41cd406901279131e0b3b333c5fa8b
Ritesh Soun <sounritesh@gmail.com>
2021-02-04 16:09:15 +0530

    model name changed

--- test_1/Products/models.py
+++ test_1/Products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Products (models.Model):
+class Product (models.Model):
     title =         models.TextField()
     description =   models.TextField()
     price =         models.TextField()
